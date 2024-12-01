2e3f14a954cf08acf21746e1e2b56b74f8294ce1
Brendenh2314 <brendenh2314@gmail.com>
2024-11-30 19:35:18 -0700

    Completed my SQL an Python projects
    Completed multiple projects and uploaded a new data tab as well as updated resume

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const toggleMenu = () =>{
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function showTab(category) {
+    const sweProjects = document.getElementById('swe-projects');
+    const dataProjects = document.getElementById('data-projects');
+    const sweButton = document.querySelector('button[onclick="showTab(\'swe\')"]');
+    const dataButton = document.querySelector('button[onclick="showTab(\'data\')"]');
+
+    if (category === 'swe') {
+        sweProjects.style.display = 'block';
+        dataProjects.style.display = 'none';
+        sweButton.classList.add('active-tab');
+        dataButton.classList.remove('active-tab');
+    } else if (category === 'data') {
+        sweProjects.style.display = 'none';
+        dataProjects.style.display = 'block';
+        sweButton.classList.remove('active-tab');
+        dataButton.classList.add('active-tab');
+    }
+}
